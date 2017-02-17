c173afe8fe9752b1dcf2f3d761ec65cde8f5ca7e
Gareth Jones <garethjns4@gmail.com>
2017-02-17 16:23:49 +0000

    Updated eyeTracker object
    - More robust TCP
    - Processing methods now handle gaze and surface

--- EyeTracker/run.py
+++ EyeTracker/run.py
@@ -17,8 +17,11 @@ class eyeTracker():
     Automatic processing uses defualts and saves data to disk and self
     Assumes one surface called target
     
-    Processsing methods are designed to be externalls callable, but not tested
-    yet.
+   
+    surfaceToPandasDF fails when trying to deal with anything that's not a 
+    surface subscription - needs updating
+     - Replaced with surfaceGazeToPandasDF for now but a more flexible 
+     implenetation would be useful
     """
     
     
@@ -39,6 +42,8 @@ class eyeTracker():
         self.processNow = processNow
         self.TCPPort = TCPPort
         self.TCPAddr = TCPAddr
+        self.timeSwapRec = np.nan
+        self.timeSwapSend = np.nan
         
         if not isinstance(subs, list):
             subs=[subs]
@@ -69,21 +74,21 @@ class eyeTracker():
         req.connect(adrStr)
         
         # Ask for the sub port
-        print '   Requesting sub port'
+        print '    Requesting sub port'
         req.send_string('SUB_PORT')
         subPort = req.recv(0)
         
         # open a sub port to listen to pupil
-        print '   Connecting to sub port:', subPort
+        print '    Connecting to sub port:', subPort
         sub = context.socket(zmq.SUB)
         sub.connect("tcp://%s:%s" %(ip, int(subPort)))
-        print 'Connected'
+        print '    Connected'
         
         # Subscribe to messages
         
         for msgs in self.subs:
             sub.setsockopt(zmq.SUBSCRIBE, msgs)
-            print 'Subscribed to', msgs
+            print '    Subscribed to', msgs
         
             
         self.sub = sub
@@ -110,31 +115,44 @@ class eyeTracker():
         try:
             # Wait for a connection
             sock.listen(1)
-            print('Waiting for MATLAB')
+            print('    Waiting for MATLAB')
             connection, client_address = sock.accept()
-            print('MATLAB connected')
+            print('    MATLAB connected')
             
             
-            data = connection.recv(11)
+            
+            # Expecting buffer of length 24. MATLAB is buffering GetSecs time
+            # with 24 zeros
+            data = connection.recv(24)
             
             # When MATLAB connects, send time
             t = repr(time.time())
             connection.send(t)
             
-            print('Times swapped')
+            print('    Times swapped')
             self.timeSwapRec = data
             self.timeSwapSend = t
             
-            connection.close()
-            sock.close()
-            print('TCP closed')
+            # Wait for an OK for MATLAB - expect length 100 to avoid data 
+            # remaining from first buffer
+            data = connection.recv(100)
             
-            if self.startNow:
-                eyeTracker.runExp(self)
-        except:
-            # Any errors? Attempt close and don't continue
             connection.close()
             sock.close()
+            print('    TCP closed')
+            
+            TCPOK=True
+        except Exception as ex:
+            # Any errors? Attempt close and don't continue
+            TCPOK = False
+            print('    TCP connection failed:')
+            print(str(ex))
+            connection.close()
+            sock.close()
+            
+            
+        if self.startNow and TCPOK:
+                eyeTracker.runExp(self)   
         
     def runExp(self):
         # Run collection of eye data and pickle to disk
@@ -166,7 +184,7 @@ class eyeTracker():
                 print "Collected at " + str(t)
                 
         except: # Catch abort
-            print 'Stopped' 
+            print '    Stopped' 
             
             # Close open file    
             f.close()      
@@ -176,9 +194,9 @@ class eyeTracker():
             
             
     def process(self):
-        
+        print('Processing')
         eyeTracker.unpickle(self)
-        eyeTracker.surfaceToPandasDF(self)
+        eyeTracker.surfaceGazeToPandasDF(self)
         
         
     def unpickle(self):
@@ -199,11 +217,12 @@ class eyeTracker():
                     # OK for now, maybe update for performance later
                     objs.append(pickle.load(f))
                 except EOFError:
+                    print('    Loaded all')
                     break
 
             
         except:
-            print('Pickle file empty.')
+            print('    Pickle file empty')
       
         
         self.objs = objs    
@@ -294,9 +313,114 @@ class eyeTracker():
          
         scio.savemat(fnOut, {'struct': dv})
         
-        print('Saved: ' + fnOut)
+        print('    Saved: ' + fnOut)
         self.df = df
         return(df)
+        
+        
+    def surfaceGazeToPandasDF(self, objs=[], surfs = ['Target'], fnOut=''):
+        import pandas as pd
+        from msgpack import loads 
+        import numpy as np
+        import scipy.io as scio
+        
+        # Convert objs containg gaze information to pandas dataframe and 
+        # save a .mat version to disk
+        # (Might be better to skip conversion to df as scio.savemat saves dicts)
+        
+        if len(fnOut)==0:
+                fnOut=self.fnOut
+            
+        if len(objs)==0:
+                objs = self.objs
+        
+        # Get n and track its for reporting
+        n = len(objs)
+        it = 0.0
+        # Prepare df (df.append perf ok?)
+        df = pd.DataFrame()
+        
+        # For each message, process JSON, append as row to df
+        for data in objs:
+            it+=1
+            
+            # Extract time stamp
+            ts = data['TS']
+            # Process message
+            msg = loads(data['msg'])
+            
+            if 'name' in msg.keys() and msg['name'] in surfs:
+                mType = 'Surf'
+                # This is a surface message
+
+                # Find requested surfaces in msg
+                # (Just 1 for now)
+                filtSurf = msg
+                # Get gaze data - may be more than one entry
+                gaze = filtSurf['gaze_on_srf']
+                gd = {'NP':[], 'on':[]}
+                for subGaze in gaze:
+                    gd['NP'].append(subGaze['norm_pos'])
+                    gd['on'].append(subGaze['on_srf'])
+                    # print subGaze['topic'], subGaze['on_srf']
+        
+                # Average available data
+                # 'on_srf'
+                # Here no data returns NaN
+                onSurf = np.nanmean(gd['on'])
+                
+                # 'norm_pos'
+                # Works for now, but needs updating
+                # Need to handle mean on [x,y]
+                # Need to handle mean on []
+                try:
+                    NP = np.nanmean(gd['NP'], axis=0)
+                    if isinstance(NP, np.float64):
+                        NP = [0,0]
+                except:
+                    NP = [0,0]
+            
+
+            if 'topic' in msg.keys() and msg['topic'] == 'gaze':
+                mType = 'Gaze'
+                # This is a gaze message
+                # There are two NPs in here
+                # msg['base_data']['norm_pos']
+                # ['norm_pos']
+                NP = msg['norm_pos']
+                onSurf = np.nan
+
+            # Report progress
+            print str(ts) + ' (' + str(it/n*100) + '%)'
+            
+            # Get save norm_pos data and TS
+            dRow = pd.DataFrame({'TS': ts, 
+                          'onSurf' : onSurf,
+                          'NP0' : NP[0],
+                          'NP1' : NP[1],
+                          'mType' : mType},
+                           index = [int(it)])
+            # Append to df
+            df = df.append(dRow)
+            
+        # Save as .mat
+        # Can't save pandas df directly, so convert to dict to be saved as 
+        # structure    
+        dv = {col : df[col].values for col in df.columns.values}   
+        
+        # Also add time data to dv
+        dv['timeSwapRec'] = self.timeSwapRec
+        dv['timeSwapSend'] = self.timeSwapSend
+        dv['creationTime'] = self.creationTime
+        dv['fn'] = self.fn 
+        dv['subs'] = self.subs
+         
+        scio.savemat(fnOut, {'struct': dv})
+        
+        print('    Saved: ' + fnOut)
+        self.df = df
+        return(df)
+        
     
 #%% Run 
 %clear
@@ -305,7 +429,8 @@ class eyeTracker():
 fn = "SyncTest"
 port = 50020
 TCPAddr = 'localhost'
-TCPPort = 52002
+TCPAddr = '128.40.249.99'
+TCPPort = 52012
 
 # Connect, wait, run, process
 eye = eyeTracker(fn=fn, port=port, subs=['surface', 'gaze'],
