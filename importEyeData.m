864dfff4282c2bcf0bcdff887e22a1b1fce217f8
garethjns <garethjns4@gmail.com>
2017-03-06 13:44:26 +0000

    Added new loadGaze function

--- importEyeData.m
+++ importEyeData.m
@@ -9,15 +9,17 @@ close all
 
 % fn = 'EyeTracker\SurfaceTest3.p.mat';
 fn = 'Data\8\02-Feb-2017 10_39_19\8.p.mat';
+fn = 'Data\GarethEye\21-Feb-2017 15_53_30\GarethEye.mat';
+
 % fn = 'Data\8\8.p.mat';
-fn = 'EyeTracker/GarethEye.mat'
-[gaze, nG] = loadGaze(fn, {'TS', 'NP0', 'NP1', 'onSurf'});
+[gaze, nG] = loadGaze(fn, {'TS', 'NP0', 'NP1', 'onSurf', 'mType'});
 
 
 %% Preprocess gaze data
 
 % Remove empty rows
-gaze = gaze(~isnan(gaze.onSurf),:);
+nanIdx =  all(isnan([gaze.NP0, gaze.NP1, gaze.onSurf]),2);
+gaze = gaze(~nanIdx,:);
 
 % % Apply threshold
 % gazeThresh = 1;
@@ -34,7 +36,8 @@ gaze.TS2 = datetime(gaze.TS, 'ConvertFrom', 'posixtime');
 close all
 
 figure
-plot(gaze.TS, gaze.onSurf)
+surfIdx = strcmp(gaze.mType, 'Surf');
+plot(gaze.TS(surfIdx), gaze.onSurf(surfIdx))
 
 plotGaze([gaze.NP0, gaze.NP1], gaze.onSurf, 'RawGaze')
 
@@ -165,6 +168,7 @@ end
 gazePropThresh = 0.5;
 
 fn = 'Data\8\02-Feb-2017 10_39_19\SpatialCapture_8.mat';
+fn = 'Data\GarethEye\21-Feb-2017 15_53_30\SpatialCapture_GarethEye.mat';
 % fn = 'Data\8\8.p.mat';
 a = load(fn);
 stimLog = a.stimLog(~isnan(a.stimLog.PosBin(:,1)),:);
@@ -174,25 +178,92 @@ nT = height(stimLog);
 stimLog.sTime = datetime(stimLog.startClock);
 stimLog.eTime = datetime(stimLog.endClock);
 
+%% Plot trials and raw gaze
+
 figure
 hold on
 % Add gaze data to stimLog
 stimLog.onSurf = NaN(nT,1);
 stimLog.nGazeSamples = NaN(nT,1);
 stimLog.onSurfProp = NaN(nT,1);
+stimLog.nGazeSamplesAfterThisTrial = NaN(nT,1);
+stimLog.onSurfPropAfterThisTrial = NaN(nT,1);
+% For each trial
+% Get gaze prop during trial, and gaze prop after trial
 for r = 1:nT
-   ts = stimLog.sTime(r);
-   te = stimLog.eTime(r);
-   
-   tIdx = gazeCorrected.TS2>=ts & gazeCorrected.TS2<=te;
-   
-   gs = gazeCorrected.onSurfED(tIdx);
-   
-   stimLog.nGazeSamples(r) = numel(gs);
-   stimLog.onSurfProp(r) = mean(gs);
-   
-   plot([ts,te], [1.01, 1.01], 'LineWidth', 3)
+    
+    % disp(num2str(r))
+    
+    ts = stimLog.sTime(r);
+    te = stimLog.eTime(r);
+    if r == nT
+        tsNext = stimLog.sTime(r)+0.0000001;
+    else
+        tsNext = stimLog.sTime(r+1);
+    end
+    
+    tIdx = gaze.TS2>=ts & gaze.TS2<=te;
+    tNextIdx = gaze.TS2>te & gaze.TS2<tsNext;
+    
+    % gs = gazeCorrected.onSurfED(tIdx);
+    gs = gaze.onSurf(tIdx);
+    gsGap = gaze.onSurf(tNextIdx);
+    
+    stimLog.nGazeSamples(r) = numel(gs);
+    stimLog.onSurfProp(r) = nanmean(gs);
+    stimLog.nGazeSamplesAfterThisTrial(r) = numel(gsGap);
+    stimLog.onSurfPropAfterThisTrial(r) = nanmean(gsGap);
+    
+    plot([ts,te], [1.001, 1.001], 'LineWidth', 3)
+    plot([ts,te], [stimLog.onSurfProp(r), stimLog.onSurfProp(r)], 'LineWidth', 3, 'Color',  'k')
+    plot([te,tsNext], [stimLog.onSurfPropAfterThisTrial(r), stimLog.onSurfPropAfterThisTrial(r)], 'LineWidth', 3, 'Color',  'r')
+    % drawnow
 end
 
-plot(stimLog.sTime, stimLog.onSurfProp)
+% plot(stimLog.sTime, stimLog.onSurfProp)
+% plot(stimLog.sTime, stimLog.onSurfProp>=gazePropThresh)
+
+%% Plot trials and corrected gaze
+
+figure
+hold on
+% Add gaze data to stimLog
+stimLog.gazeCorrectedOnSurf = NaN(nT,1);
+stimLog.nGazeCorrectedSamples = NaN(nT,1);
+stimLog.gazeCorrectedOnSurfProp = NaN(nT,1);
+stimLog.nGazeCorrectedSamplesAfterThisTrial = NaN(nT,1);
+stimLog.gazeCorrectedOnSurfPropAfterThisTrial = NaN(nT,1);
+% For each trial
+% Get gaze prop during trial, and gaze prop after trial
+for r = 1:nT
+    
+    % disp(num2str(r))
+    
+    ts = stimLog.sTime(r);
+    te = stimLog.eTime(r);
+    if r == nT
+        tsNext = stimLog.sTime(r)+0.0000001;
+    else
+        tsNext = stimLog.sTime(r+1);
+    end
+    
+    tIdx = gazeCorrected.TS2>=ts & gazeCorrected.TS2<=te;
+    tNextIdx = gazeCorrected.TS2>te & gazeCorrected.TS2<tsNext;
+    
+    % gs = gazeCorrected.onSurfED(tIdx);
+    gs = gazeCorrected.onSurf(tIdx);
+    gsGap = gazeCorrected.onSurf(tNextIdx);
+    
+    stimLog.nGazeCorrectedSamples(r) = numel(gs);
+    stimLog.gazeCorrectedOnSurfProp(r) = mean(gs);
+    stimLog.nGazeCorrectedSamplesAfterThisTrial(r) = numel(gsGap);
+    stimLog.gazeCorrectedOnSurfPropAfterThisTrial(r) = mean(gsGap);
+    
+    plot([ts,te], [1.001, 1.001], 'LineWidth', 3)
+    plot([ts,te], [stimLog.gazeCorrectedOnSurfProp(r), stimLog.gazeCorrectedOnSurfProp(r)], 'LineWidth', 3, 'Color',  'k')
+    plot([te,tsNext], [stimLog.gazeCorrectedOnSurfPropAfterThisTrial(r), stimLog.gazeCorrectedOnSurfPropAfterThisTrial(r)], 'LineWidth', 3, 'Color',  'r')
+    % drawnow
+end
+
+% plot(stimLog.sTime, stimLog.onSurfProp)
 % plot(stimLog.sTime, stimLog.onSurfProp>=gazePropThresh)
